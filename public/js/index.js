92b182f7d150b6d887e7bd2324707b2fd7ec7047
unknown <atul.aggrawal97@gmail.com>
2018-09-17 21:12:27 +0530

    scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,19 @@
 var socket =io();
+
+function scrollToBottom () {
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child')
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+      messages.scrollTop(scrollHeight);
+    //  console.log("should scroll");
+    }
+}
 socket.on('connect',() =>{
   console.log('connected to server');
 });
@@ -14,6 +29,7 @@ socket.on('newMessage',function(message){
     createdAt:formattedTime
   });
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
  socket.on('newLocationMessage',function (message) {
   var formattedTime =moment(message.createdAt).format('h:mm a');
@@ -24,6 +40,7 @@ socket.on('newMessage',function(message){
     createdAt:formattedTime
   });
    jQuery('#messages').append(html);
+  scrollToBottom();
  });
 jQuery('#message-form').on('submit',function (e)
 {
