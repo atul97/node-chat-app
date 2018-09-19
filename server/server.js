71224f6226480ebdd06350b102aec545b5a6dfbf
unknown <atul.aggrawal97@gmail.com>
2018-09-19 18:20:46 +0530

    add user class and updateuserlist

--- server/server.js
+++ server/server.js
@@ -3,18 +3,35 @@ const http=require('http');
 const express=require('express');
 const socketIO =require('socket.io');
 const {generateMessage,generateLocationMessage} = require('./utils/message');
-const publicpath=path.join(__dirname , '../public');
-
+const {isRealString} =require('./utils/validation.js');
+const {Users}=require('./utils/users');
+const publicPath=path.join(__dirname , '../public');
 const port=process.env.PORT || 3000;
 
  var app=express();
  var server=http.createServer(app);
 var io=socketIO(server);
+var users=new Users();
+
+app.use(express.static(publicPath));
 io.on('connection',(socket) => {
   console.log('New user connected');
-socket.emit('newMessage',generateMessage('Admin','Welcome to the chat app'));
 
-socket.broadcast.emit('newMessage',generateMessage('Admin','New user joined'));
+socket.on('join',(params,callback) => {
+  if (!isRealString(params.name) || !isRealString(params.room)) {
+    return callback('Name and room name are required');
+  }
+  socket.join(params.room);
+  users.removeUser(socket.id);
+  users.addUser(socket.id,params.name,params.room);
+
+  io.to(params.room).emit('updateUserList',users.getUserList(params.room));
+
+  socket.emit('newMessage',generateMessage('Admin','Welcome to the chat app'));
+  socket.broadcast.to(params.room).emit('newMessage',generateMessage('Admin',params.name+' has joined'));
+
+  callback();
+});
 socket.on('createMessage',(message,call) =>{
   console.log('createMessage',message);
   io.emit('newMessage',generateMessage(message.from,message.text));
@@ -27,10 +44,15 @@ socket.on('createLocationMessage',(coords) =>{
 });
 
   socket.on('disconnect', () => {
-    console.log('user was disconnected');
+    var user=users.removeUser(socket.id);
+
+    if(user ){
+      io.emit(user.room).emit('updateUserList',users.getUserList(user.room));
+      io.to(user.room).emit('newMessage',generateMessage('Admin',user.name+' has left'));
+    }
 });
 });
- app.use(express.static(publicpath));
+ //app.use(express.static(publicpath));
  server.listen(port, () => {
    console.log('server is on '+port);
  });
